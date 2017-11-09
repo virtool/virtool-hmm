28b0f5a8472d6cbf14e03c1b34832594f8a31653
igboyes <igboyes@gmail.com>
2017-11-09 15:21:21 -0800

    Fix build.py tests

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,4 +1,4 @@
-import gzip
+import filecmp
 import json
 import operator
 import os
@@ -6,8 +6,8 @@ import sys
 
 import hmm.build
 
-JSON_GZ_PATH = os.path.join(sys.path[0], "tests", "annotations.json.gz")
-ANNOTATIONS_PATH = os.path.join(sys.path[0], "annotations")
+JSON_PATH = os.path.join(sys.path[0], "tests", "annotations.json")
+ANNOTATIONS_PATH = os.path.join(sys.path[0], "tests", "annotations")
 
 
 def test_dir_to_json(tmpdir):
@@ -15,26 +15,19 @@ def test_dir_to_json(tmpdir):
     Test that the collection of vFam text files is correctly translated to JSON.
 
     """
-    output_path = os.path.join(str(tmpdir), "test.json.gz")
+    output_path = os.path.join(str(tmpdir), "test.json")
 
     hmm.build.dir_to_json(ANNOTATIONS_PATH, output_path)
 
     assert os.path.isfile(output_path)
 
-    with gzip.open(output_path, "rt") as json_file:
-        output = json.load(json_file)
+    with open(output_path) as f:
+        output = json.load(f)
 
-    to_check = [{key: r[key] for key in r if key not in ["entries", "genera", "families"]} for r in output]
-    to_check = sorted(to_check, key=operator.itemgetter("cluster"))
+    with open(JSON_PATH) as f:
+        expected = json.load(f)
 
-    expected_annotations = [
-        {"length": 133, "mean_entropy": 0.53, "count": 208, "cluster": 5, "total_entropy": 70.49},
-        {"length": 356, "mean_entropy": 0.52, "count": 253, "cluster": 2, "total_entropy": 185.12},
-        {"length": 96, "mean_entropy": 0.56, "count": 210, "cluster": 4, "total_entropy": 53.76},
-        {"length": 505, "mean_entropy": 0.52, "count": 113, "cluster": 10, "total_entropy": 262.6},
-        {"length": 612, "mean_entropy": 0.52, "count": 101, "cluster": 8, "total_entropy": 318.24},
-        {"length": 136, "mean_entropy": 0.53, "count": 216, "cluster": 3, "total_entropy": 72.08},
-        {"length": 500, "mean_entropy": 0.47, "count": 97, "cluster": 9, "total_entropy": 235.0}
-    ]
+    for data in (output, expected):
+        data = sorted(data, key=operator.itemgetter("cluster"))
 
-    assert all(x == y for x, y in zip(to_check, expected_annotations))
+    assert all(x == y for x, y in zip(output, expected))
