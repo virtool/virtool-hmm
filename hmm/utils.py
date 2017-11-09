35027310127674c65a42f04d7cd15119cfad5ec9
igboyes <igboyes@gmail.com>
2017-11-09 12:05:09 -0800

    Update utils.py module

--- hmm/utils.py
+++ hmm/utils.py
@@ -2,14 +2,14 @@ import os
 import subprocess
 
 
-def prune_annotations(hmm_path, annotations_path, mock=True):
+def prune_annotations(profiles_path, annotations_path, mock=True):
     clusters = [int(f.split("_")[1]) for f in os.listdir(annotations_path)]
+
+    return clusters
 
 
 def hmmstat(path):
-    command = ["hmmstat", path]
-
-    output = await loop.run_in_executor(None, subprocess.check_output, command)
+    output = subprocess.check_output(["hmmstat", path])
 
     result = [line.split() for line in output.decode("utf-8").split("\n") if line and line[0] != "#"]
 
---
+++ tests/test_utils.py
@@ -0,0 +1,6 @@
+import hmm.utils
+
+
+def test_prune_annotations():
+    pass
+    # hmm.utils.prune_annotations(profiles_path, annotations_path, mock=True)
